057f6ef0aa3046efc53dd72d6d32676a7fad1eb0
Geovanny <birdcode.py@gmail.com>
2024-03-05 14:11:21 -0500

    Creation of the table of hummingbirds species in Valle del Cauca, according to GBIF database

--- bio_valle_cauca_col.sql
+++ bio_valle_cauca_col.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION postgis;
 
-SELECT species, family, COUNT(DISTINCT id) AS gbif_rec FROM trochilidae_val
-GROUP BY species, family ORDER BY gbif_rec DESC;
+SELECT species, genus, COUNT(DISTINCT id) AS gbif_rec FROM trochilidae_val
+GROUP BY species, genus ORDER BY gbif_rec DESC;
