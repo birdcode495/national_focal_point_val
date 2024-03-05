5ae22b06445f5e61c23503698719b1ff09891dff
Geovanny <birdcode.py@gmail.com>
2024-03-05 16:01:50 -0500

    Label creation 1

--- bio_valle_cauca_col.sql
+++ bio_valle_cauca_col.sql
@@ -1,10 +1,21 @@
+-- Creation of the postgis extension in the National 
+-- Focal Point Valle del Cauca database
+
 CREATE EXTENSION postgis;
+
+-- Creation of the table of hummingbird species sighted in Valle del Cauca, 
+-- according to the information published by the GBIF
 
 SELECT species, genus, COUNT(DISTINCT id) AS gbif_rec FROM trochilidae_val
 GROUP BY species, genus ORDER BY gbif_rec DESC;
+
+-- Creation of the hummingbird species richness table by municipality 
+-- of Valle del Cauca
 
 SELECT mpio_cnmbr AS municipality, COUNT(DISTINCT species) AS richness_humming
 FROM municipalities_val, trochilidae_val 
 WHERE ST_Intersects(municipalities_val.geom, trochilidae_val.geom)
 GROUP BY municipality ORDER BY richness_humming DESC;
+
+
 
