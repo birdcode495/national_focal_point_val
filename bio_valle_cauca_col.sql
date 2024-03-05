f5b065e76ba3759be8c55fbe76081157ffb920d1
Geovanny <birdcode.py@gmail.com>
2024-03-05 15:49:10 -0500

    Creation of table of hummingbirds richness per municipality of Valle del Cauca

--- bio_valle_cauca_col.sql
+++ bio_valle_cauca_col.sql
@@ -2,3 +2,9 @@ CREATE EXTENSION postgis;
 
 SELECT species, genus, COUNT(DISTINCT id) AS gbif_rec FROM trochilidae_val
 GROUP BY species, genus ORDER BY gbif_rec DESC;
+
+SELECT mpio_cnmbr AS municipality, COUNT(DISTINCT species) AS richness_humming
+FROM municipalities_val, trochilidae_val 
+WHERE ST_Intersects(municipalities_val.geom, trochilidae_val.geom)
+GROUP BY municipality ORDER BY richness_humming DESC;
+
